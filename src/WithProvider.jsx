9ad6c7dc7f39b8b61b8e0a456c138b005c9c71d6
Praveen-Rangar <praveenrangarkamand@gmail.com>
2022-11-11 00:08:52 +0530

    again error

--- src/WithProvider.jsx
+++ src/WithProvider.jsx
@@ -1,12 +1,10 @@
 import React, { useContext } from "react";
 import { AlertContext, userContext } from "./Contexts";
 
-const  WithProvider = (provider) =>   (IncomingComponent) =>  (props) => {
-      const contextData = useContext(provider);
-      return <IncomingComponent {...props} {...contextData} />;
-    };
-  };
-
+const WithProvider = (provider) => (IncomingComponent) => (props) => {
+  const contextData = useContext(provider);
+  return <IncomingComponent {...props} {...contextData} />;
+};
 
 export default WithProvider;
 
